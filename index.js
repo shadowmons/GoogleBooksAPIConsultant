b02c379c64159e1ad89d01bc7f3b42176bf36797
Victor Gil <victor.e.g.b2@gmail.com>
2023-11-28 11:05:01 -0500

    Post request configuration, includind the standar parameter of the request

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 let APIkey= "ede";
+const URL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 
 app.use(express.static("public"));
@@ -20,25 +21,42 @@ app.post("/submit", (req, res) => {
   res.redirect("/");
   
 });
+  
+  app.post("/search", async (req, res) => {
+    console.log(req.body);
+    let URLsearch = URL + req.body["volume"].replace(/ /g, '+');
+    if (req.body["special"]!='No keyword'){
+      URLsearch = URLsearch + "+" + req.body["special"]+ ":" +req.body["keyword"];
+    }
 
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-  
-});
-  
-  app.get("/ewfew", async (req, res) => {
-    res.render("index.ejs");
-      try {
-          const result = await axios.get("https://secrets-api.appbrewery.com/random");
-          const data = result.data;
-          res.render("index.ejs", { secret: data.secret, user: data.username });
-        } catch (error) {
-          res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-        }
+    if (req.body["filter"]!='no filter'){
+      URLsearch = URLsearch + "&filter=" + req.body["filter"];
+    }
+
+    if (req.body["printType"]!='no filter'){
+      URLsearch = URLsearch + "&printType=" + req.body["printType"];
+    }
+
+    if (req.body["sorting"]!='No defined'){
+      URLsearch = URLsearch + "&orderBy=" + req.body["sorting"];
+    }
+    
+    URLsearch = URLsearch + "&key=" +APIkey;
+    console.log(URLsearch);
+
+    try {
+      const result = await axios.get(URLsearch);
+      const data = result.data;
+      console.log(data);
+      res.render("index.ejs", res.locals={APIkey:APIkey,results: JSON.stringify(data) });
+      console.log("se logro")
+    } catch (error) {
+      res.render("index.ejs", { APIkey:APIkey, results: JSON.stringify(error.response.data) });
+    }
     });
 
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+  
   
