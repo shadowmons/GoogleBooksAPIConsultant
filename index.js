7aa4d5f73e7f94153ee978e59b83b38d19580c9f
Victor Gil <victor.e.g.b2@gmail.com>
2023-11-27 22:25:58 -0500

    adding bootstrap and ccs components

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+const APIKey = "AIzaSyCaGRRS827WanJDqgAuP92b6kuNb6VAeZg";
+//key=AIzaSyCaGRRS827WanJDqgAuP92b6kuNb6VAeZg
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-    try {
-        const result = await axios.get("https://secrets-api.appbrewery.com/random");
-        const data = result.data;
-        res.render("index.ejs", { secret: data.secret, user: data.username });
-      } catch (error) {
-        res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-      }
+  res.render("index.ejs");
   });
+  
+  app.get("/ewfew", async (req, res) => {
+    res.render("index.ejs");
+      try {
+          const result = await axios.get("https://secrets-api.appbrewery.com/random");
+          const data = result.data;
+          res.render("index.ejs", { secret: data.secret, user: data.username });
+        } catch (error) {
+          res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+        }
+    });
 
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
